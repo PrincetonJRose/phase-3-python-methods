a38e5538bb812979a64c36ce63ed8b8f82174730
Princeton J. Rose <princetonjrose@gmail.com>
2022-12-19 13:15:37 -0500

    finished practice

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,20 @@
 
 def greet_programmer():
     pass
+    print( "Hello, programmer!" )
 
 def greet(name):
+    print( f"Hello, {name}!" )
     pass
 
 def greet_with_default(name="programmer"):
+    print( f"Hello, {name}!" )
     pass
 
 def add(num1, num2):
     pass
+    return num1 + num2
 
 def halve(number):
     pass
+    return number / 2
